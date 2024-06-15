1aadd8f26fc8a92a8915d5370d888b843563a047
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2024-06-15 14:47:49 +0200

    Update HomeView.jsx

--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useAuth } from "../../context/AuthContext";
 import { AplicacionCard } from "../card/AplicacionCard";
 import { AplicacionUrlCard } from "../card/AplicacionUrlCard";
@@ -6,10 +6,27 @@ import { AplicacionUrlCard } from "../card/AplicacionUrlCard";
 function HomeView({setVentana}) {
    const auth = useAuth();
   const {displayName} = auth.user
+  const [vpn, setVpn] = useState(false);
+  const response = fetch("https://https-filtering-check.adtidy.org/");
+  if(response=="OK"){
+    setVpn(true);
+  }
+  
 
 
   return <>
       <h1>Hola {displayName}</h1>
+      <hr/>
+      <button type="button" class="btn position-relative">
+        AdGuard
+        {vpn ? <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+          <span class="visually-hidden"></span>
+        </span> : <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+          <span class="visually-hidden"></span>
+        </span>}
+        
+      </button>
+      
       <hr />
       <div className="row row-cols-1 row-cols-md-3 g-4">
         <AplicacionUrlCard nombre="Gasolineras" version="0.1"/>
@@ -28,5 +45,6 @@ function HomeView({setVentana}) {
       
   </>
 }
+
 
 export default HomeView;
