733f859cdb482ca916606ef929ca646bc8430e18
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2024-06-23 12:13:11 +0200

    1.0

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth"
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrUKdkGkaBkPtWFQDF9BkiC-_FEH0ZWoo",
-    authDomain: "ibaicolegio-d38fa.firebaseapp.com",
-    projectId: "ibaicolegio-d38fa",
-    storageBucket: "ibaicolegio-d38fa.appspot.com",
-    messagingSenderId: "430038539994",
-    appId: "1:430038539994:web:a6fe926c9ded3292ce8fe3",
-    measurementId: "G-RQVJB447Z6"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
